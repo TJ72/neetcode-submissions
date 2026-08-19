391386e8fe4a5488581d72002a5b39308393e99d
Albert Tseng <tjtseng1072@gmail.com>
2026-08-19 10:08:36 +0800

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-0

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        target = k * threshold
+        currSum = sum(arr[:k])
+        res = 1 if currSum >= target else 0
+
+        for r in range(k, len(arr)):
+            currSum = currSum - arr[r - k] + arr[r]
+            if currSum >= target:
+                res += 1
+        
+        return res
